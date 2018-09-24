61acbfc0ad042bedae0fb01fb5b010806c45a8b0
Zach VanSomeren <23041336+zvansom@users.noreply.github.com>
2018-09-24 13:55:33 -0700

    Update 04multiply_by.py

--- exercises/04multiply_by.py
+++ exercises/04multiply_by.py
@@ -1,9 +1,9 @@
 # Write a method called `multiply_by` that takes a number and
-# array, and returns the array of numbers multiplied by that number.
+# a list, and returns the array of numbers multiplied by that number.
 #
 # You'll want to apply your fundamental programming knowledge here.
 # What are the pieces to this problem? You'll need to define a method,
-# need a return statement, need a for loop to iterate over the array.
+# need a return statement, need a for loop to iterate over the list.
 #
 # Example method call:
 #
